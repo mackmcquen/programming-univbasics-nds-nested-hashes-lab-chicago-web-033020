23316cec406e32a724fe49beb405047a75b29830
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 20:29:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  array = 
+  array = [
+    {name}]
 end
 
 def aoh_lookup(aoh, row, key)
